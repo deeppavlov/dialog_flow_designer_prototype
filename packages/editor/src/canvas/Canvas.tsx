0e65bec6bc5f98c5d6a2ef1cd5d5b4c423998b91
mablin7 <magyarb0201@gmail.com>
2022-06-08 16:26:13 +0200

    feat(editor): improved zooming and panning

--- packages/editor/src/canvas/Canvas.tsx
+++ packages/editor/src/canvas/Canvas.tsx
@@ -1,4 +1,5 @@
-import { FC, useEffect, useRef, useState } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
+import * as Rematrix from "rematrix";
 import FloatingNode from "./FloatingNode";
 import { nodeHeight, nodeWidth } from "./Node";
 import Edge from "./Edge";
@@ -7,7 +8,6 @@ import { useStore } from "../store";
 import shallow from "zustand/shallow";
 import pick from "../utils/pick";
 
-const zoomSpeed = 0.99;
 const maxZoom = 1;
 const minZoom = 0.1;
 
@@ -19,32 +19,58 @@ const Canvas: FC = () => {
 
   const canvasRef = useRef<HTMLDivElement>(null);
   const viewportRef = useRef<HTMLDivElement>(null);
-  let { nodes, edges } = graph;
+  const { nodes, edges } = graph;
   const [jumping, setJumping] = useState(false);
-  const [pan, setPan] = useState([0, 0]);
-  const [zoom, setZoom] = useState(1);
+  const [viewTransform, setTransfrom] = useState<Rematrix.Matrix3D>(Rematrix.identity());
 
+  /**
+   * Applies the given transforms to the current transforms, **from left to right**.
+   */
+  const applyTransforms = (...matrices: Rematrix.Matrix3D[]) =>
+    setTransfrom((prevTransform) =>
+      [prevTransform, ...matrices]
+        .reverse() // We reverse so arguments can be given in more intuitive, l->r order
+        .reduce(Rematrix.multiply)
+    );
+
+  // Jump to selected node
   useEffect(() => {
-    if (!selectedNodeId || !viewportRef.current) return;
-    const selectedPos = nodeLayoutPositions[selectedNodeId];
-    const selectedCenterX = pan[0] + (selectedPos.x + nodeWidth / 2) * zoom;
-    const selectedCenterY = pan[1] + (selectedPos.y + nodeHeight / 2) * zoom;
-    const { width, height } = viewportRef.current.getBoundingClientRect();
-    const dx = width / 2 - selectedCenterX;
-    const dY = height / 2 - selectedCenterY;
-    setJumping(true);
-    setPan(([x, y]) => [x + dx, y + dY]);
-  }, [selectedNodeId]);
+    setTransfrom((prevTrans) => {
+      if (!selectedNodeId || !viewportRef.current || !canvasRef.current) return prevTrans;
+      // Get the view's offset relative to the canvas origin
+      const { x: viewClientX, y: viewClientY } = viewportRef.current.getBoundingClientRect();
+      const { x: canvasX, y: canvasY, width, height } = canvasRef.current.getBoundingClientRect();
+      const viewXInCanvas = viewClientX - canvasX;
+      const viewYInCanvas = viewClientY - canvasY;
 
+      // Get the selected node's offset relative to the canvas origin
+      const zoom = prevTrans[/* scale */ 0];
+      const selectedPos = nodeLayoutPositions[selectedNodeId];
+      const xInCanvas = viewXInCanvas + (selectedPos.x + nodeWidth / 2) * zoom;
+      const yInCanvas = viewYInCanvas + (selectedPos.y + nodeHeight / 2) * zoom;
+
+      // Calculate offset to jump
+      const dX = width / 2 - xInCanvas;
+      const dY = height / 2 - yInCanvas;
+      setJumping(true);
+      return [prevTrans, Rematrix.translate(dX, dY)].reverse().reduce(Rematrix.multiply);
+    });
+  }, [nodeLayoutPositions, selectedNodeId]);
+
+  const handleTransitionEnd = () => setJumping(false);
+
+  // Handle panning
   const handlePointerMove: React.PointerEventHandler = (ev) => {
     if (ev.buttons !== 1) return;
     ev.preventDefault();
     ev.stopPropagation();
-    setPan(([x, y]) => [x + ev.movementX, y + ev.movementY]);
+    applyTransforms(Rematrix.translate(ev.movementX, ev.movementY));
   };
 
+  // Handle zooming
   const handleWheel: React.WheelEventHandler = (ev) => {
-    const { deltaY } = ev;
+    const { deltaY, clientX, clientY } = ev;
+    const zoom = viewTransform[/* scale */ 0];
     if (
       !viewportRef.current ||
       !canvasRef.current ||
@@ -52,19 +78,15 @@ const Canvas: FC = () => {
       (zoom <= minZoom && deltaY > 0)
     )
       return;
-
-    const dZoom = Math.pow(zoomSpeed, deltaY / 10);
+    const zoomMul = deltaY < 0 ? 1.1 : 0.9;
     const canvasRect = canvasRef.current.getBoundingClientRect();
-    const viewRect = viewportRef.current.getBoundingClientRect();
-    const currentCenterX = viewRect.x + canvasRect.width / 2;
-    const currentCenterY = viewRect.y + canvasRect.height / 2;
-    const mousePosToCurrentCenterDistanceX = ev.clientX - currentCenterX;
-    const mousePosToCurrentCenterDistanceY = ev.clientY - currentCenterY;
-    const dX = mousePosToCurrentCenterDistanceX * (1 - dZoom);
-    const dY = mousePosToCurrentCenterDistanceY * (1 - dZoom);
-
-    setZoom((z) => z * dZoom);
-    setPan(([x, y]) => [x + dX, y + dY]);
+    const mouseX = clientX - canvasRect.x;
+    const mouseY = clientY - canvasRect.y;
+    applyTransforms(
+      Rematrix.translate(-mouseX, -mouseY),
+      Rematrix.scale(zoomMul),
+      Rematrix.translate(mouseX, mouseY)
+    );
   };
 
   return (
@@ -76,10 +98,10 @@ const Canvas: FC = () => {
     >
       <div
         ref={viewportRef}
-        className={cn("h-full w-full", jumping && "transition-transform")}
-        onTransitionEnd={() => setJumping(false)}
+        className={cn("h-full w-full relative origin-top-left", jumping && "transition-transform")}
+        onTransitionEnd={handleTransitionEnd}
         style={{
-          transform: `translate(${pan[0]}px, ${pan[1]}px) scale(${zoom})`,
+          transform: Rematrix.toString(viewTransform),
         }}
       >
         <svg
