9b8d09ab89f2ca49bdff620665a7b2095e12885e
mablin7 <magyarb0201@gmail.com>
2022-06-08 14:04:44 +0200

    misc: setup eslint

--- packages/editor/.eslintrc.js
+++ packages/editor/.eslintrc.js
@@ -2,9 +2,25 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  plugins: ["react", "@typescript-eslint"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
   ignorePatterns: ["media", "scripts"],
   rules: {
     semi: [2, "always"],
---
+++ packages/extension/.eslintrc.js
@@ -0,0 +1,31 @@
+/**@type {import('eslint').Linter.Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "@typescript-eslint"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+  ],
+  ignorePatterns: ["media", "scripts"],
+  rules: {
+    semi: [2, "always"],
+    "@typescript-eslint/no-unused-vars": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "@typescript-eslint/no-non-null-assertion": 0,
+  },
+};
