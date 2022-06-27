2a9a1d45ff55b01eabf6b6076dbafee89427dd3e
mablin7 <magyarb0201@gmail.com>
2022-06-27 18:44:03 +0200

    misc: streamline plot2Graph

--- packages/editor/src/utils/plot.ts
+++ packages/editor/src/utils/plot.ts
@@ -1,9 +1,15 @@
 import { Graph, GNode, Turn, GEdge } from "../types";
 import { NodeType, Plot } from "@dialog-flow-designer/shared-types/df-parser-server";
 
+/**
+ * Convert a parsed DF plot in the format outputted by the parser to a graph (nodes and edges).
+ *
+ * @see https://github.com/kudep/dff-parser/blob/896dae2e86f5e78b75dc51bae7423394e3c21529/parser_output.yaml
+ */
 export const plotToGraph = (plot: Plot): Graph => {
-  const edgeId = ([src, trg]: [string, string]) => `e${src}-${trg}`;
-  const globalEdges: any[] = [];
+  // Code for extracting global nodes
+  // How exactly these should be rendered is currently TBD from a design perspective
+  // const globalEdges: any[] = [];
   // Object.values(plot.nodes)
   //   .filter(({ type }) => type === "global")
   //   .flatMap((node) => node.transitions ?? [])
@@ -16,17 +22,40 @@ export const plotToGraph = (plot: Plot): Graph => {
   //   }));
 
   const edgeIds = new Set<string>();
-  const edges: [string, string][] = [];
-  const transitions: { id: string; cond: string }[] = [
-    ...globalEdges.map(({ id, cond }) => ({ id, cond })),
-  ];
+  const edges: GEdge[] = [];
+  /**
+   * Add an edge to the edges list, if it does not exists yet
+   */
+  const addEdge = (srcId: string, trgId: string) => {
+    const id = `e${srcId}-${trgId}`;
+    if (!edgeIds.has(id)) {
+      edgeIds.add(id);
+      edges.push({
+        fromId: `${srcId}`,
+        toId: `${trgId}`,
+      });
+    }
+  };
+
+  const nodes: GNode[] = [];
   Object.entries(plot.nodes).forEach(([id, node]) => {
     if (node.type !== "regular" || !node.transitions) return;
     node.transitions?.forEach((transId) => {
       const trans = plot.transitions[transId];
       if (!trans.label.startsWith("id#nd")) return;
 
-      // Find condition
+      // 1. Add current response node
+      nodes.push({
+        id,
+        label: node.name || "noname",
+        turn: Turn.BOT,
+        properties: [],
+      });
+
+      // 2. Add edge from node to condition
+      addEdge(id, transId);
+
+      // 3. Find condition name/type
       let cond = "unknown";
       const condId = trans.condition;
       if (condId.startsWith("id#ln")) {
@@ -38,71 +67,18 @@ export const plotToGraph = (plot: Plot): Graph => {
         }
       }
 
-      // Condition node
-      transitions.push({ id: transId, cond });
+      // 4. Add condition node
+      nodes.push({
+        id,
+        label: cond,
+        turn: Turn.USER,
+        properties: [],
+      });
 
-      // Edge from node to condition
-      const eid1 = edgeId([id, transId]);
-      if (!edgeIds.has(eid1)) {
-        edgeIds.add(eid1);
-        edges.push([id, transId]);
-      }
-
-      // Edge from condition to target node
-      const eid2 = edgeId([transId, trans.label]);
-      if (!edgeIds.has(eid2)) {
-        edgeIds.add(eid2);
-        edges.push([transId, trans.label]);
-      }
-    });
-
-    globalEdges.forEach(({ id: transId, cond, target }) => {
-      if (target === id) return;
-      // Edge from node to condition
-      const eid1 = edgeId([id, transId]);
-      if (!edgeIds.has(eid1)) {
-        edgeIds.add(eid1);
-        edges.push([id, transId]);
-      }
-      // Edge from condition to target
-      const eid2 = edgeId([transId, target]);
-      if (!edgeIds.has(eid2)) {
-        edgeIds.add(eid2);
-        edges.push([transId, target]);
-      }
+      // 5. Add edge from condition to target node
+      addEdge(transId, trans.label);
     });
   });
 
-  return {
-    nodes: [
-      ...transitions.map(
-        ({ id, cond }) =>
-          <GNode>{
-            id,
-            label: cond,
-            turn: Turn.USER,
-            properties: {},
-          }
-      ),
-      ...Object.entries(plot.nodes)
-        .filter(([_, node]) => node.type === "regular")
-        .map(
-          ([id, node]) =>
-            <GNode>{
-              id,
-              label: node.name || "noname",
-              turn: Turn.BOT,
-              properties: {},
-            }
-        ),
-    ],
-    edges: edges.map(
-      ([srcId, trgId]) =>
-        <GEdge>{
-          id: `e${srcId}-${trgId}`,
-          fromId: `${srcId}`,
-          toId: `${trgId}`,
-        }
-    ),
-  };
+  return { nodes, edges };
 };
